1a88e886077e517af4bffc58f601898b70a4e4dd
Luka Simović <40174491+LukaSimovic@users.noreply.github.com>
2021-10-05 00:00:53 +0200

    Update skripta.sh

--- skripta.sh
+++ skripta.sh
@@ -1,6 +1,6 @@
-./zadatak1/asembler -o ./zadatak1/tests/interrupts.o ./zadatak1/tests/interrupts.s
-./zadatak1/asembler -o ./zadatak1/tests/main.o ./zadatak1/tests/main.s
+./asembler/asembler -o ./asembler/tests/interrupts.o ./asembler/tests/interrupts.s
+./asembler/asembler -o ./asembler/tests/main.o ./asembler/tests/main.s
 
-./zadatak2/linker -hex -place=ivt@0x0000 -o ./zadatak2/tests/program.hex ./zadatak1/tests/interrupts.o ./zadatak1/tests/main.o
+./linker/linker -hex -place=ivt@0x0000 -o ./linker/tests/program.hex ./asembler/tests/interrupts.o ./asembler/tests/main.o
 
-./zadatak3/emulator ./zadatak2/tests/program.hex
+./emulator/emulator ./linker/tests/program.hex
